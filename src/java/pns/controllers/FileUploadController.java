bee1f27dc08b4e3a0fa919f694837d9c9f8d524f
User <User@192.168.10.10>
2017-11-27 12:05:07 +0300

    24-11-17  File Placing To DB

--- src/java/pns/controllers/FileUploadController.java
+++ src/java/pns/controllers/FileUploadController.java
@@ -49,134 +49,142 @@ public class FileUploadController implements Runnable {
 
     @PostConstruct
     public void init() {
-	rooot = fa.getAppRootPath(true);
+        rooot = fa.getAppRootPath(true);
 
-	fsa.setIsAppSubDirPath(true);
-	fsa.setFileDir("satdata");
-	generatePath();
+        fsa.setIsAppSubDirPath(true);
+        fsa.setFileDir("satdata");
+        generatePath();
     }
 
     public String getFContent() {
-	return fContent;
+        return fContent;
     }
 
     public String getUploadDir() {
-	return uploadDir;
+        return uploadDir;
     }
 
     public String getfContent() {
-	return fContent;
+        return fContent;
     }
 
     public String getSucsessUplMSG() {
-	return sucsessUplMSG;
+        return sucsessUplMSG;
     }
 
     public String getErrorUplMSG() {
-	return errorUplMSG;
+        return errorUplMSG;
     }
 
     public String getDestination() {
-	return destination;
+        return destination;
     }
 
     public UploadedFile getUploadedFile() {
-	return uploadedFile;
+        return uploadedFile;
     }
 
     public void setUploadedFile(UploadedFile uploadedFile) {
-	this.uploadedFile = uploadedFile;
+        this.uploadedFile = uploadedFile;
     }
 
     public void generatePath() {
-	destination = fa.getFullFileName();
+        destination = fa.getFullFileName();
     }
 
     public void generatePath(String dirName) {
-	fsa.fullFilePath(dirName);
-	destination = fa.getFullFileName();
+        fsa.fullFilePath(dirName);
+        destination = fa.getFullFileName();
     }
 
     public void generatePath(String dirName, String resultFName) {
-	fa.fullFilePath(dirName, resultFName);
-	destination = fa.getFullFileName();
+        fa.fullFilePath(dirName, resultFName);
+        destination = fa.getFullFileName();
     }
 
     public void setSelectedFile(File selectedFile) {
-	this.selectedFile = selectedFile;
+        this.selectedFile = selectedFile;
+    }
+
+    public String getRootDir() {
+        return rootDir;
+    }
+
+    public String getRooot() {
+        return rooot;
     }
 
     public File getSelectedFile() {
-	return selectedFile;
+        return selectedFile;
     }
 
     public long getProgress() {
-	return progress;
+        return progress;
     }
 
     public String getFinUploadTo() {
-	return finUploadTo;
+        return finUploadTo;
     }
 
     @Override
     public void run() {
-	if (uploadedFile != null) {
-	    copyFile(fContent);
-	}
-	try {
-	    throw new InterruptedException();
-	} catch (InterruptedException ex) {
-	}
+        if (uploadedFile != null) {
+            copyFile(fContent);
+        }
+        try {
+            throw new InterruptedException();
+        } catch (InterruptedException ex) {
+        }
     }
 
     public String getFSelectedContent() {
-	return fSelectedContent;
+        return fSelectedContent;
     }
 
     public void generateFileContent(File f) {
-	if (f != null) {
-	    String fullName = f.getAbsolutePath();
-	    if (fa.fileRead(fullName)) {
-		//System.out.println("    @@@@@   " + fullName);
-		fSelectedContent = fa.getFileContent();
-		//System.out.println("  " + fSelectedContent);
-	    }
-	}
+        if (f != null) {
+            String fullName = f.getAbsolutePath();
+            if (fa.fileRead(fullName)) {
+                //System.out.println("    @@@@@   " + fullName);
+                fSelectedContent = fa.getFileContent();
+                //System.out.println("  " + fSelectedContent);
+            }
+        }
     }
 
     public String outputFileContent(File f) {
-	if (f != null) {
-	    fa = new FileActor();
-	    String fullName = f.getAbsolutePath();
-	    if (fa.fileRead(fullName)) {
-		//System.out.println("    @@@@@   " + fullName);
-		return fa.getStringBuffer().toString();
-		//System.out.println("  " + fSelectedContent);
-	    }
-	}
-	fa = null;
-	return "; ";
+        if (f != null) {
+            fa = new FileActor();
+            String fullName = f.getAbsolutePath();
+            if (fa.fileRead(fullName)) {
+                //System.out.println("    @@@@@   " + fullName);
+                return fa.getStringBuffer().toString();
+                //System.out.println("  " + fSelectedContent);
+            }
+        }
+        fa = null;
+        return "; ";
     }
 
     public void upload() {
-	if (uploadedFile == null) {
-	    errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
-		    + languageBean.translateMSG("EmptyFile");
-	    return;
-	}
+        if (uploadedFile == null) {
+            errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
+                    + languageBean.translateMSG("EmptyFile");
+            return;
+        }
 
-	boolean isUploaded = realUploadFile();
-	if (isUploaded) {
-	    sucsessUplMSG = languageBean.translateMSG("FileUploaded") + ": " + destination;
+        boolean isUploaded = realUploadFile();
+        if (isUploaded) {
+            sucsessUplMSG = languageBean.translateMSG("FileUploaded") + ": " + destination;
 //            th.start();
 
-	    if (uploadedFile != null) {
-		copyFile(fContent);
-	    }
-	} else {
-	    errorUplMSG += ".  " + languageBean.translateMSG("FileDoesNotUploaded");
-	    //" Error while Uploading: The file format is not supporting or file size is too lage... " + (uploadedFile.getSize() / 1024) + " KB ";
-	}
+            if (uploadedFile != null) {
+                copyFile(fContent);
+            }
+        } else {
+            errorUplMSG += ".  " + languageBean.translateMSG("FileDoesNotUploaded");
+            //" Error while Uploading: The file format is not supporting or file size is too lage... " + (uploadedFile.getSize() / 1024) + " KB ";
+        }
 
 //        try {
 ////            runBush.execute(args);
@@ -190,31 +198,31 @@ public class FileUploadController implements Runnable {
 
     private void generateFinalPathUpload(String ext) {
 
-	generateRootUploadPath(); // generate a rootDirName
+        generateRootUploadPath(); // generate a rootDirName
 
-	String prefaceName
-		= pns.utils.dateTimeMechanism.convertLongToDateStr(System.currentTimeMillis(),
-			"yyyyMMddHHmm_", true);
-	prefaceName = pns.utils.RStrings.rndString(4, 'A', 'Z') + "_" + prefaceName;
-	String instrument = "";   //"i_" + pns.utils.RSrings.rndString(3, 'A', 'S');
-	String fileName = uploadedFile.getFileName();
-	String fileProperName = fileName.split("\\.")[0];
+        String prefaceName
+                = pns.utils.dateTimeMechanism.convertLongToDateStr(System.currentTimeMillis(),
+                        "yyyyMMddHHmm_", true);
+        prefaceName = pns.utils.RStrings.rndString(4, 'A', 'Z') + "_" + prefaceName;
+        String instrument = "";   //"i_" + pns.utils.RSrings.rndString(3, 'A', 'S');
+        String fileName = uploadedFile.getFileName();
+        String fileProperName = fileName.split("\\.")[0];
 
-	String dir = fsa.setTemplateTimePath("YYYY/MM-dd", true);
-	destination = "/" + fsa.getFileDir();
-	destination += "/" + dir;
-	destination = rootDir + destination;
-	destination = FileActor.getAbsoluteFileName(destination);
-	finUploadTo = instrument = instrumentIDCreator(fContent);
+        String dir = fsa.setTemplateTimePath("YYYY/MM-dd", true);
+        destination = "/" + fsa.getFileDir();
+        destination += "/" + dir;
+        destination = rootDir + destination;
+        destination = FileActor.getAbsoluteFileName(destination);
+        finUploadTo = instrument = instrumentIDCreator(fContent);
 // _suspicious
-	System.out.println("  ~~~~~~~~~~~~>>fileName " + fileName + ";    " + uploadedFile.getFileName() + "  ***  " + fileName.contains("_suspicious"));
-	if (fileName.contains("_suspicious")) {
-	    finUploadTo += "_suspicious";
-	}
+        System.out.println("  ~~~~~~~~~~~~>>fileName " + fileName + ";    " + uploadedFile.getFileName() + "  ***  " + fileName.contains("_suspicious"));
+        if (fileName.contains("_suspicious")) {
+            finUploadTo += "_suspicious";
+        }
 
-	finUploadTo = prefaceName + "_" + finUploadTo + "." + ext;
+        finUploadTo = prefaceName + "_" + finUploadTo + "." + ext;
 
-	System.out.println("generateFinalPathUpload  **************>>fileName " + finUploadTo + ";    ");
+        System.out.println("generateFinalPathUpload  **************>>fileName " + finUploadTo + ";    ");
 
     }
 
@@ -222,115 +230,115 @@ public class FileUploadController implements Runnable {
      * Generating rootDirName
      */
     private void generateRootUploadPath() {
-	if (uploadedFile.getSize() > 1024 * 1024 * 2) {
-	    errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
-		    + languageBean.translateMSG("TheFileSizeIsTooLage") + " "
-		    + (uploadedFile.getSize() / 1024) + " KB ";
-	    return;
-	}
-	String prefaceName
-		= pns.utils.dateTimeMechanism.convertLongToDateStr(System.currentTimeMillis(),
-			"yyyyMMddHHmm_", true);
-	// FileName for writing uploaded file
+        if (uploadedFile.getSize() > 1024 * 1024 * 2) {
+            errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
+                    + languageBean.translateMSG("TheFileSizeIsTooLage") + " "
+                    + (uploadedFile.getSize() / 1024) + " KB ";
+            return;
+        }
+        String prefaceName
+                = pns.utils.dateTimeMechanism.convertLongToDateStr(System.currentTimeMillis(),
+                        "yyyyMMddHHmm_", true);
+        // FileName for writing uploaded file
 
-	// FolderName for writing file
-	rootDir = fa.getRootPath() + destination;
-	rootDir = FileActor.getAbsoluteFileName(rootDir);
+        // FolderName for writing file
+        rootDir = fa.getRootPath() + destination;
+        rootDir = FileActor.getAbsoluteFileName(rootDir);
 
-	System.out.println(" ~~~~~  rootDir  " + rootDir);
+        System.out.println(" ~~~~~  rootDir  " + rootDir);
 
     }
 
     public boolean realUploadFile() {
 
-	if (uploadedFile.getSize() > 1024 * 1024 * 2) {
-	    errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
-		    + languageBean.translateMSG("TheFileSizeIsTooLage")
-		    + "... " + (uploadedFile.getSize() / 1024) + " KB ";
-	    return false;
-	}
-	try {
-	    long startRead = System.currentTimeMillis();
-	    InputStream is = uploadedFile.getInputstream();
-	    //int read = 0;
-	    // byte[] bytes = new byte[2048 * 3];
-	    String[] uplParts = uploadedFile.getFileName().split("\\.");
-	    String uplExt = uplParts[uplParts.length - 1];
+        if (uploadedFile.getSize() > 1024 * 1024 * 2) {
+            errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
+                    + languageBean.translateMSG("TheFileSizeIsTooLage")
+                    + "... " + (uploadedFile.getSize() / 1024) + " KB ";
+            return false;
+        }
+        try {
+            long startRead = System.currentTimeMillis();
+            InputStream is = uploadedFile.getInputstream();
+            //int read = 0;
+            // byte[] bytes = new byte[2048 * 3];
+            String[] uplParts = uploadedFile.getFileName().split("\\.");
+            String uplExt = uplParts[uplParts.length - 1];
 
-	    Scanner sc = new Scanner(uploadedFile.getInputstream(), "cp1251");
-	    long LNG = 0;
-	    String line = "";
-	    while (sc.hasNext()) {
-		String currLine = sc.nextLine() + System.lineSeparator();
-		//currLine = new String(currLine.getBytes(), "cp1251");
-		System.out.println("   " + currLine);
-		fContent += currLine;
-		//progress = calcProgress(currLine);
-		LNG++;
-	    }
-	    if (!kiam.utils.parsers.GeneralValidation.validateProtocols(fContent)) {
+            Scanner sc = new Scanner(uploadedFile.getInputstream(), "cp1251");
+            long LNG = 0;
+            String line = "";
+            while (sc.hasNext()) {
+                String currLine = sc.nextLine() + System.lineSeparator();
+                //currLine = new String(currLine.getBytes(), "cp1251");
+                System.out.println("   " + currLine);
+                fContent += currLine;
+                //progress = calcProgress(currLine);
+                LNG++;
+            }
+            if (!kiam.utils.parsers.GeneralValidation.validateProtocols(fContent)) {
 
-		errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
-			+ languageBean.translateMSG("FileFormatIsNotSupporting");
+                errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
+                        + languageBean.translateMSG("FileFormatIsNotSupporting");
 //                        " Error while Uploading: The file format is not supporting";
-		fContent = "  Error file content: " + fContent;
-		return false;
-	    }
-	    generateRootUploadPath();
+                fContent = "  Error file content: " + fContent;
+                return false;
+            }
+            generateRootUploadPath();
 
-	    DirectoryDeepGo ddg = new DirectoryDeepGo();
-	    ddg.setRootDir(rootDir);
-	    ddg.setDirToInvestigate(fsa.getFileDir());
-	    ddg.goDeep();
-	    generateFinalPathUpload(uplExt);
+            DirectoryDeepGo ddg = new DirectoryDeepGo();
+            ddg.setRootDir(rootDir);
+            ddg.setDirToInvestigate(fsa.getFileDir());
+            ddg.goDeep();
+            generateFinalPathUpload(uplExt);
 //
 //            System.out.println("   ---------->destination: " + destination);
-	    System.out.println("   ----> finUploadTo:  " + finUploadTo);
+            System.out.println("   ----> finUploadTo:  " + finUploadTo);
 
-	    System.out.println(" ---------------------->  ddg.getRootDir(): " + ddg.getRootDir());
-	    boolean eqa = false;// ddg.checkEqualDataInFiles(fContent, true);
-	    //System.out.println("     ~~~~~~~~~    eqa: " + eqa);
-	    if (eqa) {
+            System.out.println(" ---------------------->  ddg.getRootDir(): " + ddg.getRootDir());
+            boolean eqa = false;// ddg.checkEqualDataInFiles(fContent, true);
+            //System.out.println("     ~~~~~~~~~    eqa: " + eqa);
+            if (eqa) {
 
-		errorUplMSG = " Error while Uploading: The file with such content already exists ";
-		errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
-			+ languageBean.translateMSG("FileExists");
+                errorUplMSG = " Error while Uploading: The file with such content already exists ";
+                errorUplMSG = languageBean.translateMSG("ErrorWhileUploading") + ": "
+                        + languageBean.translateMSG("FileExists");
 
-		fContent = "Error file content: " + System.lineSeparator() + fContent;
-		return false;
-	    }
+                fContent = "Error file content: " + System.lineSeparator() + fContent;
+                return false;
+            }
 
-	    System.out.println(System.lineSeparator() + "rooot!:   " + rooot + System.lineSeparator() + " =============" + System.lineSeparator());
-	    String FFN = destination + "/" + finUploadTo;
-	    fsa.setFileDir(destination);
-	    fsa.setScriptName(rooot + "/scriptS/simplesend.sh");
+            System.out.println(System.lineSeparator() + "rooot!:   " + rooot + System.lineSeparator() + " =============" + System.lineSeparator());
+            String FFN = destination + "/" + finUploadTo;
+            fsa.setFileDir(destination);
+            fsa.setScriptName(rooot + "/scriptS/simplesend.sh");
 
-	    fsa.setAppDir(rootDir);
-	    fsa.setFullFileName(FFN);
+            fsa.setAppDir(rootDir);
+            fsa.setFullFileName(FFN);
 
-	} catch (IOException ex) {
-	    Logger.getLogger(FileUploadController.class.getName()).log(Level.SEVERE, null, ex);
-	}
-	return true;
+        } catch (IOException ex) {
+            Logger.getLogger(FileUploadController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return true;
     }
 
     /**
      * stoppp!
      */
     public void copyFile(String contentString) {
-	String logpath = fsa.getAppDir() + "/satlog/";
-	//File flog = new File(logpath + "/satlog");
+        String logpath = fsa.getAppDir() + "/satlog/";
+        //File flog = new File(logpath + "/satlog");
 
-	boolean bCopy
-		= fsa.fileWrite(contentString, "660", rooot + "/scriptS/uploadLOG");
-	//fsa.fileWrite(contentString, "660", flog.getAbsolutePath());
-	//fsa.fileWrite(contentString, false, true, true);
-	//fsa.fileWrite(contentString);
-	if (bCopy) {
-	    sucsessUplMSG = languageBean.translateMSG("FileUploaded") + "!! ";
-	} else {
-	    errorUplMSG = languageBean.translateMSG("UnableToCopyTo") + "  " + destination;
-	}
+        boolean bCopy
+                = fsa.fileWrite(contentString, "660", rooot + "/scriptS/uploadLOG");
+        //fsa.fileWrite(contentString, "660", flog.getAbsolutePath());
+        //fsa.fileWrite(contentString, false, true, true);
+        //fsa.fileWrite(contentString);
+        if (bCopy) {
+            sucsessUplMSG = languageBean.translateMSG("FileUploaded") + "!! ";
+        } else {
+            errorUplMSG = languageBean.translateMSG("UnableToCopyTo") + "  " + destination;
+        }
 
     }
 
@@ -341,30 +349,30 @@ public class FileUploadController implements Runnable {
      * @return
      */
     private String instrumentIDCreator(String str) {
-	str = str.trim();
-	String res = "";
-	String[] parts = str.split(System.lineSeparator());
-	String[] subParts = parts[0].split("\\s");
-	res = subParts.length + "_";
-	if (subParts.length == 2) {
-	    res = subParts[0];
-	} else if (subParts.length == 3) {
-	    res = subParts[1];
-	} else if (subParts.length == 9) {
-	    try {
-		subParts = parts[1].split("\\s");
-		res = subParts[0];
-	    } catch (ArrayIndexOutOfBoundsException ex) {
-	    }
+        str = str.trim();
+        String res = "";
+        String[] parts = str.split(System.lineSeparator());
+        String[] subParts = parts[0].split("\\s");
+        res = subParts.length + "_";
+        if (subParts.length == 2) {
+            res = subParts[0];
+        } else if (subParts.length == 3) {
+            res = subParts[1];
+        } else if (subParts.length == 9) {
+            try {
+                subParts = parts[1].split("\\s");
+                res = subParts[0];
+            } catch (ArrayIndexOutOfBoundsException ex) {
+            }
 
-	}
-	return res;
+        }
+        return res;
     }
 
     private long calcProgress(String line) {
-	long result = 0;
-	byte[] bytes = line.getBytes();
-	result = bytes.length / uploadedFile.getSize();
-	return result;
+        long result = 0;
+        byte[] bytes = line.getBytes();
+        result = bytes.length / uploadedFile.getSize();
+        return result;
     }
 }
--- src/java/pns/entity/controllers/FileViewController.java
+++ src/java/pns/entity/controllers/FileViewController.java
@@ -7,6 +7,7 @@ package pns.entity.controllers;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -18,7 +19,9 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
@@ -32,19 +35,21 @@ import javax.persistence.criteria.Root;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 import pns.controllers.FileDownloadController;
+import pns.controllers.FileUploadController;
 import pns.entity.User;
 import pns.fileUtils.FileActor;
 import pns.fileUtils.FileSpecActor;
 import pns.kiam.entities.satellites.FileMeasured;
 import pns.kiam.entities.telescopes.Telescope;
+import pns.kiam.filecontrol.FileMeasuredController;
 
 /**
  *
  * @author User
  */
 @Named
-@SessionScoped
-public class FileViewController extends AbstractEntityController {
+@RequestScoped
+public class FileViewController {
 
     private List<FileMeasured> fmList = new ArrayList<>();
     private FileMeasured fmCurr;
@@ -60,6 +65,12 @@ public class FileViewController extends AbstractEntityController {
 
     @Inject
     private FileDownloadController fdc;
+
+    @Inject
+    private FileUploadController fuc;
+
+    @Inject
+    private FileMeasuredController fmc;
 
     @PostConstruct
     public void init() {
@@ -124,6 +135,7 @@ public class FileViewController extends AbstractEntityController {
 
     public void setFilterValue(String filterValue) {
         this.filterValue = filterValue;
+        System.out.println("   4444   555  555 55 " + filterValue);
     }
 
     public void recDownload(FileMeasured fm) {
@@ -151,7 +163,21 @@ public class FileViewController extends AbstractEntityController {
     }
 
     public void createArchiveREC() {
-        //xxparser.getFileMeasuredController().readArchiveFileDir();
+        String rroot = fuc.getRooot() + "/satdata";
+        System.out.println("  rroot " + rroot);
+        fmc.setArchPath(rroot);
+        List<FileMeasured> fml = fmc.readArchiveFileDir();
+        System.out.println("  fml    fml.size()  " + fml.size());
+        for (int k = 0; k < fml.size(); k++) {
+            FileMeasured tmpf = fml.get(k);
+            System.out.println("  file " + tmpf.getFileName());
+            System.out.println(" exists " + fmList.contains(tmpf));
+//            if (!fmList.contains(tmpf)) {
+//                emA.getTransaction().begin();
+//                emA.persist(tmpf);
+//                emA.getTransaction().commit();
+//            }
+        }
     }
 
     public String uploadMomentUTC(FileMeasured fm) {
@@ -162,6 +188,7 @@ public class FileViewController extends AbstractEntityController {
     }
 
     public void filterOutput() {
+        System.out.println("    public void filterOutput(): filterValue " + filterValue);
         if (filterValue.length() == 0 || filterValue == null) {
             return;
         }
